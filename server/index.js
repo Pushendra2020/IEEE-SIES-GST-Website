68d76257b376e4f0a791c49e9969843b4b34fd12
Pushendra2020 <pushendravedak2020@gmail.com>
2025-09-05 14:53:49 +0530

    For the server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-import connectDB from "./database/db.database.js";
-import app from "./app.js";
-import dotenv from 'dotenv'
+// import connectDB from "./database/db.database.js";
+// import app from "./app.js";
+// import dotenv from 'dotenv'
 
-dotenv.config({
-    path:'./.env'
-})
+// dotenv.config({
+//     path:'./.env'
+// })
 
-connectDB()
-.then(()=>{
-    // app.listen(process.env.PORT || 3000,()=>{
-    //     console.log(`The PORT is running on ${process.env.PORT}`)
-    // })
-    app.get(`${process.env.ORIGIN1}/api/hello`, (req, res) => {
-  res.json({ message: "Hello from Express on Vercel!" });
-});
-}).catch((error)=>{
-    console.log("Failed to Connect :( ",error)
-})
+// connectDB()
+// .then(()=>{
+//     // app.listen(process.env.PORT || 3000,()=>{
+//     //     console.log(`The PORT is running on ${process.env.PORT}`)
+//     // })
+//     app.get(`${process.env.ORIGIN1}/api/hello`, (req, res) => {
+//   res.json({ message: "Hello from Express on Vercel!" });
+// });
+// }).catch((error)=>{
+//     console.log("Failed to Connect :( ",error)
+// })
 
-app.get('/', (req, res) => {
-    res.send('Backend is running');
-});
+// app.get('/', (req, res) => {
+//     res.send('Backend is running');
+// });
